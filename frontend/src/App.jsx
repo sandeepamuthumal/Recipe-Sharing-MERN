8e4be83240c5465861a18f652e0847f797f18865
sandeepamuthumal <sandeepamuthumal3@gmail.com>
2025-02-13 01:22:51 +0530

    frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+import Recipes from "./pages/Recipes";
+function App() {
+  return (
+    <Container className="mt-5">
+      <Recipes/>
+    </Container>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Recipes.jsx
@@ -0,0 +1,8 @@
+
+export default function Recipes() {
+  return (
+    <>
+        <h1 className="text-center text-primary">Welcome to Recipe Book</h1>
+    </>
+  )
+}
